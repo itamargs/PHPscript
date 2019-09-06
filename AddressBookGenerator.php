90f3905ed7ccbccba6ce14208ca169e2feb692b4
itamar <itamargs@gmail.com>
2019-09-06 15:15:20 +0300

    ADD: Bonus feature + Some error handling
    * Added image of persons from google photo search
    * Added more error handling
    * Get csv file as parameter instead of hardcoded

--- AddressBookGenerator.php
+++ AddressBookGenerator.php
@@ -36,6 +36,7 @@ function generatePersonCard()
 
     $personCardContent = '
 <?php
+// Get data about persons from the html link
     if(isset($_GET["first"]) && isset($_GET["last"]) && isset($_GET["phone"])
         && isset($_GET["street"]) && isset($_GET["city"]) && isset($_GET["zip"]))
     {
@@ -46,9 +47,33 @@ function generatePersonCard()
         $city = $_GET["city"];
         $zip = $_GET["zip"];
     }
+    
+// Get person image from google potso search
+// A bit tricky since google Search photo API via json deprecated since 2011 and didnt want to use API key here
+// Mehod: search with keywords-> get html content -> use REGEX to get the specific image link appropriate 
+    $search_keyword = $first." ".$last; //Key word for searching photo
+$search_keyword=str_replace(\' \',\'+\',$search_keyword);
+$url = "https://www.google.com/search?q=".$search_keyword."&tbm=isch";
+//get string with html data of the content of requested keywords
+$input = @file_get_contents($url) or die("Could not access file: $url");
+//build REGEX to get only the link to the first photo (best matching) encrypted (small Thumbnail sized photo)
+$regexp = \'"https://encrypted-tbn0.gstatic.com(.*?)"\';
+if (preg_match_all("~".$regexp."~", $input, $result)) {
+    $matches = $result[0];
+    $photoUrl =  $matches[0];
+} else
+    print "nothing found";
+echo \'
+<!DOCTYPE html>
+<html>
+<body>
+<img src=\'.$photoUrl. \' alt="Trulli" width="150" height="130">
+</body>
+</html>
+    \';
+    
 Echo "<html>";
 Echo
-    "<p>Person card:</p>".
     "<p> First Name: " . $first. "</p>".
     "<p> Last Name: " . $last. "</p>".
     "<p> phone number: " . $phone. "</p>".
@@ -61,10 +86,44 @@ Echo
     fclose($f);
 }
 
-//------------------   </Functions>  ----------------------
+// Collect warnings and threw errors
+set_error_handler(
+    function ($severity, $message, $file, $line) {
+        throw new ErrorException($message, $severity, $severity, $file, $line);
+    }
+);
+
+//-----------------------   </Functions>  ----------------------
+
+//todo: check path other then in same folder as php
+// ------------------------- Running from HERE ---------------------------
+//check if CSV file passed as first argument into script
+if ($argc == 2) {
+    $file_parts = pathinfo($argv[1]);
+    if ($file_parts['extension'] != "csv") {
+        echo "Accepting only csv files \n";
+        echo "EXIT \n";
+        exit();
+    }
+} else {
+    echo "USAGE: php -f AddressBookGenerator.php filename.csv \n";
+    echo "EXIT \n";
+    exit();
+}
+echo "Input is OK.\nRUNNING...\n";
+
 
 //creating array from the csv file
-$csvArray = array_map('str_getcsv', file('addressBook.csv'));
+
+try {
+    $csvArray = array_map('str_getcsv', file($argv[1]));
+}
+catch (Exception $e) {
+    echo "ERROR: CSV FIle not found or in wrong format \n";
+    echo "EXIT";
+    exit();
+}
+restore_error_handler();
 $firstLineArray = $csvArray[0]; //save the first row in the array
 
 
@@ -97,12 +156,12 @@ for ($i = 0; $i < count($csvArray); $i++) { //iterates rows
     $rowHolder = $csvArray[$i];
     $words = ""; // resets after each row to collect the words from the next row
     for ($j = 0; $j < count($rowHolder); $j++) { //iterates words in each row
-        if($j == 0){ //then we in the 'first name' value and need to hyper link it
+        if ($j == 0) { //then we in the 'first name' value and need to hyper link it
             //Attaching the person data to send it later combined with HTML link into our "person card"
-            $words = '<a href=" ./personCard.php?'. "first=".$rowHolder[0]."&last=".$rowHolder[1]."&phone="
-            .$rowHolder[2]."&street=".$rowHolder[3]."&city=".$rowHolder[4]."&zip=".$rowHolder[5].'"'. ' target="_blank"'.">".$rowHolder[$j] ."</a>" . " | ";
-        }
-        else { // attach the none 'first name' values to show later in the html
+            $words = '<a href=" ./personCard.php?' . "first=" . $rowHolder[0] . "&last=" . $rowHolder[1] . "&phone="
+                . $rowHolder[2] . "&street=" . $rowHolder[3] . "&city=" . $rowHolder[4] . "&zip=" . $rowHolder[5] . '"'
+                . ' target="_blank"' . ">" . $rowHolder[$j] . "</a>" . " | ";
+        } else { // attach the none 'first name' values to show later in the html
             $words = $words . $rowHolder[$j] . ' | '; //building the rows
         }
     }
@@ -122,6 +181,8 @@ $htmlContent =
 generateHtml($htmlContent);
 // injecting content of 'person card' php page and generates it
 generatePersonCard();
+
+echo "Complete\n";
 
 
 
